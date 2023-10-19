4bc7c37a6e15e5ea997f7cc850c7d65042528713
persi0815 <jina_8151010@naver.com>
2023-10-19 13:03:25 +0900

    2023.10.19
    1. hello, world 출력

--- app/src/main/java/com/example/memorycat/MainActivity.kt
+++ app/src/main/java/com/example/memorycat/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    print("hello, world")
+
+}
+fun main() {
+    println("hello, world")
 }
