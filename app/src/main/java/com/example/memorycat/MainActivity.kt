bab25bc6ac3a4ef15ca59939c4ba030791a19974
girin-sj <radianttwilight24@gmail.com>
2023-11-07 03:11:50 +0900

    2023.11.07
    1. 하단바 완료(mypage랑 연동까지)
    2. 하단바와 상단바 구분 선 추가

--- app/src/main/java/com/example/memorycat/MainActivity.kt
+++ app/src/main/java/com/example/memorycat/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.memorycat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import navigation.DetailViewFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener(object : NavigationBarView.OnItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                // Handle item selection here
+                when (item.itemId) {
+                    R.id.action_mypage -> {
+                        var detailViewFragment = DetailViewFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+    }
 }
