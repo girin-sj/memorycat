78f5d8735d7de9c5ed7a9f708777b74772a91ae0
persi0815 <jina_8151010@naver.com>
2023-11-07 09:02:15 +0900

    2023.11.07 오늘의 영단어, 북마크 화면 간 이동
    YJW 내 화면 간 이동

--- app/src/main/java/com/example/memorycat/TodayWordStartActivity.kt
+++ app/src/main/java/com/example/memorycat/TodayWordStartActivity.kt
@@ -1,11 +1,21 @@
 package com.example.memorycat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.memorycat.databinding.ActivityTodaywordStartBinding
 
 class TodayWordStartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_todayword_start)
+        //setContentView(R.layout.activity_todayword_start)
+        val binding = ActivityTodaywordStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent_1 = Intent(this, TodayWordStudyActivity::class.java)
+        binding.studyStartButton.setOnClickListener{startActivity(intent_1)}
+
+        val intent_2 = Intent(this, WrongWordStudyActivity::class.java)
+        binding.studyStartButton.setOnClickListener{startActivity(intent_2)}
     }
 }
