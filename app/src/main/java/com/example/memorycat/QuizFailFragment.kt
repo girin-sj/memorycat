55a401449efd59d273665da8e1120f978d4f9158
seoyoung030121 <148404567+seoyoung030121@users.noreply.github.com>
2023-11-16 01:17:03 +0900

    11.16 아이콘 변경, 퀴즈 기능 db 연결
    1. 어플 아이콘, 이름을 기본 아이콘에서 변경(매니페스트 수정)
    2. 퀴즈 기능: userDB에서 사용자의 level을 가져와 level에 맞는 영단어 랜덤 출력

--- app/src/main/java/com/example/memorycat/QuizFailFragment.kt
+++ app/src/main/java/com/example/memorycat/QuizFailFragment.kt
@@ -5,34 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.memorycat.databinding.FragmentQuizFailBinding
 import com.example.memorycat.databinding.FragmentQuizResultBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-
-class QuizResultFragment : Fragment() {
-    private var _binding: FragmentQuizResultBinding? = null
+class QuizFailFragment : Fragment() {
+    private var _binding: FragmentQuizFailBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentQuizResultBinding.inflate(inflater, container, false)
+        _binding = FragmentQuizFailBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.quizNextButton.setOnClickListener {
+        binding.quizNoteButton.setOnClickListener {
             val transaction = activity?.supportFragmentManager?.beginTransaction()
-            transaction?.replace(R.id.main_content, TodayWordStartFragment())
+            transaction?.replace(R.id.main_content, QuizNoteFragment())
             transaction?.addToBackStack(null)
             transaction?.commit()
         }
-        binding.quizAgainButton.setOnClickListener {
+        binding.goTodaywordButton.setOnClickListener {
             val transaction = activity?.supportFragmentManager?.beginTransaction()
-            transaction?.replace(R.id.main_content, QuizStartFragment())
+            transaction?.replace(R.id.main_content, TodayWordStartFragment())
             transaction?.addToBackStack(null)
             transaction?.commit()
         }
