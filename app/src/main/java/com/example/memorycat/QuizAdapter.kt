d1d49578c36e8507e533d012d17d996372bcbe39
seoyoung030121 <148404567+seoyoung030121@users.noreply.github.com>
2023-11-21 20:04:49 +0900

    11.21 퀴즈뷰모델 수정

--- app/src/main/java/com/example/memorycat/QuizAdapter.kt
+++ app/src/main/java/com/example/memorycat/QuizAdapter.kt
@@ -1,19 +1,31 @@
-package com.example.memorycat
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.memorycat.databinding.ItemNoteBinding
 
-class MyViewHolder(val binding: ItemNoteBinding): RecyclerView.ViewHolder(binding.root)
+class MyViewHolder(val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root)
 
-class MyAdapter(val datas: MutableList<String>):
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MyAdapter(val datas: MutableList<String>) :
+    RecyclerView.Adapter<MyViewHolder>() {
+
+    // var onItemClick: ((Int) -> Unit)? = null
+
     override fun getItemCount(): Int = datas.size
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-        MyViewHolder(ItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val binding = (holder as MyViewHolder).binding
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder =
+        MyViewHolder(
+            ItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val binding = holder.binding
+        binding.wordData.text = datas[position]
+
+        /*
+        // Set click listener
+        holder.itemView.setOnClickListener {
+            onItemClick?.invoke(position)
+        }
+         */
     }
 }
